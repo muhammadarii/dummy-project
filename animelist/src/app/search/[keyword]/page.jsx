45384fb54e59e12f806e6b89119f6110e5aad880
muhammadarii <aripurnomo606@gmail.com>
2024-05-16 14:25:30 +0700

    Sesion 5 updated

--- animelist/src/app/search/[keyword]/page.jsx
+++ animelist/src/app/search/[keyword]/page.jsx
@@ -1,11 +1,11 @@
 import Header from "@/components/AnimeList/Header";
 import AnimeList from "@/components/AnimeList";
+import { getAnimeResponse } from "@/app/libs/api-libs";
 
 const Page = async ({params}) => {
     const { keyword } = params
     const decodeKeyword = decodeURI(keyword)
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodeKeyword}`)
-    const searchAnime= await response.json()
+    const searchAnime = await getAnimeResponse("anime", `q=${decodeKeyword}`)
 
   return (
     <> 
